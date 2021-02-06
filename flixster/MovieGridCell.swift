85c8def5c6b3f7b1a3406a654982ae3ad573147e
Vincent Yu <vyu1@hawk.iit.edu>
2021-02-05 21:40:11 -0600

    movie grid!
    added more features

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by suiyan he on 2/5/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
